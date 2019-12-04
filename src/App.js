5c01fa64dc8f1505aaeccbea18abbef855811359
jackdo69 <jackie.duc.do@gmail.com>
2019-12-04 20:27:31 +1100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DrumPad from './DrumPad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 'snare', letter: 'Q', src: 'https://www.myinstants.com/media/sounds/snare.mp3' },
+  { id: 'bass 1', letter: 'W', src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3' },
+  { id: 'bongo', letter: 'E', src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav' },
+  { id: 'tom tom', letter: 'A', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
+  { id: 'bass 3', letter: 'S', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
+  { id: 'shotgun', letter: 'D', src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav' },
+  { id: 'high hat', letter: 'Z', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
+  { id: 'drum hit', letter: 'X', src: 'http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3' },
+  { id: 'laser', letter: 'C', src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
+]
+
+class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display:'Click or Press a pad'
+    }
+    this.handleDisplay = this.handleDisplay.bind(this)
+  }
+
+  handleDisplay = display => this.setState({display})
+
+  render() {
+    return(
+      <div id="drum-machine">
+        <div id="display">
+        <h3>{this.state.display}</h3>
+
+        </div>
+        <div id="drum-pads">
+          {data.map(d => (
+            <DrumPad
+            key={d.id}
+            id={d.id}
+            letter={d.letter}
+            src={d.src}
+            handleDisplay={this.handleDisplay} />
+            ))}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default DrumMachine;
---
+++ src/DrumPad.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class DrumPad extends React.Component {
+
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeydown)
+    window.focus()
+  }
+
+  componenWillUnmounf() {
+    document.removeEventListener('keydown', this.handleKeydown)
+  }
+
+  handleKeydown = e => {
+    if(e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play()
+      this.audio.currentTime = 0
+      this.props.handleDisplay(this.props.id)
+    }
+  }
+
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+    this.props.handleDisplay(this.props.id)
+  }
+
+  render() {
+    return (
+      <div
+        className="drum-pad"
+        id={this.props.id}
+        onClick={this.handleClick}
+        >
+        <h1>{this.props.letter}</h1>
+        <audio
+          id={this.props.letter}
+          className="clip"
+          src={this.props.src}
+          ref={ref => this.audio = ref}
+          ></audio>
+        </div>
+    )
+  }
+}
+
+export default DrumPad;
